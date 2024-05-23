5c848fc79619f1eab2101b0dfb1bc60578b6b370
VHall1 <victorhallpsn@hotmail.com>
2024-05-23 19:38:50 +0100

    cache github responses

--- app/utils/github.server.ts
+++ app/utils/github.server.ts
@@ -1,10 +1,33 @@
 import { Octokit as createOctokit } from "@octokit/core";
 import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
 import { throttling } from "@octokit/plugin-throttling";
-
-const safePath = (s: string) => s.replace(/\\/g, "/");
+import { LRUCache } from "lru-cache";
+import {
+  cachified,
+  type CacheEntry,
+  type Cache,
+  totalTtl,
+} from "@epic-web/cachified";
 
 const Octokit = createOctokit.plugin(restEndpointMethods, throttling);
+
+const lruInstance = new LRUCache<string, CacheEntry>({ max: 1000 });
+
+const lru: Cache = {
+  set(key, value) {
+    const ttl = totalTtl(value?.metadata);
+    return lruInstance.set(key, value, {
+      ttl: ttl === Infinity ? undefined : ttl,
+      start: value?.metadata?.createdTime,
+    });
+  },
+  get(key) {
+    return lruInstance.get(key);
+  },
+  delete(key) {
+    return lruInstance.delete(key);
+  },
+};
 
 const octokit = new Octokit({
   auth: process.env.CONTENT_GITHUB_TOKEN,
@@ -32,6 +55,7 @@ export async function downloadCMSFiles(relativeFileOrDirectory: string) {
   return files.filter(({ path }) => !path.endsWith("$schema.json"));
 }
 
+const safePath = (s: string) => s.replace(/\\/g, "/");
 async function downloadDirectory(
   dir: string
 ): Promise<{ path: string; content: string }[]> {
@@ -57,32 +81,41 @@ async function downloadDirectory(
 }
 
 async function downloadFileBySha(sha: string) {
-  const { data } = await octokit.rest.git.getBlob({
-    owner: "vhall1",
-    repo: "remix-portfolio",
-    file_sha: sha,
+  return cachified({
+    key: `file-${sha}`,
+    cache: lru,
+    async getFreshValue() {
+      const { data } = await octokit.rest.git.getBlob({
+        owner: "vhall1",
+        repo: "remix-portfolio",
+        file_sha: sha,
+      });
+      const encoding = data.encoding as Parameters<typeof Buffer.from>["1"];
+      return Buffer.from(data.content, encoding).toString();
+    },
+    // 15 minutes
+    ttl: 900_000,
   });
-  const encoding = data.encoding as Parameters<typeof Buffer.from>["1"];
-  return Buffer.from(data.content, encoding).toString();
 }
 
 async function downloadDirList(path: string) {
-  const resp = await octokit.rest.repos.getContent({
-    owner: "vhall1",
-    repo: "remix-portfolio",
-    ref: "main",
-    path,
-    headers: {
-      "Cache-Control": "max-age: 3600",
+  return cachified({
+    key: `dir-${path}`,
+    cache: lru,
+    async getFreshValue() {
+      const resp = await octokit.rest.repos.getContent({
+        owner: "vhall1",
+        repo: "remix-portfolio",
+        ref: "main",
+        path,
+      });
+      const data = resp.data;
+      if (!Array.isArray(data)) {
+        throw new Error(
+          `GitHub did not return an array of files while trying to download content from ${path}.`
+        );
+      }
+      return data;
     },
   });
-  const data = resp.data;
-
-  if (!Array.isArray(data)) {
-    throw new Error(
-      `GitHub did not return an array of files while trying to download content from ${path}.`
-    );
-  }
-
-  return data;
 }
