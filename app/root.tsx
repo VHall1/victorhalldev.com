2d57110ba38e313c585cd26b7d238e52709101e0
VHall1 <victorhallpsn@hotmail.com>
2024-07-10 15:20:23 +0100

    supress hydration warning on body

--- app/root.tsx
+++ app/root.tsx
@@ -48,7 +48,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className={cn({ dark: theme === "dark" })}>
+      <body className={cn({ dark: theme === "dark" })} suppressHydrationWarning>
         {children}
         <ScrollRestoration />
         {disableScripts ? null : <Scripts />}
