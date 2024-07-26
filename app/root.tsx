87a831e5d58b614903c999f4e8041cdd841d3684
VHall1 <victorhallpsn@hotmail.com>
2024-07-26 13:44:25 +0100

    add local tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -9,13 +9,15 @@ import {
   useMatches,
   useRouteLoaderData,
 } from "@remix-run/react";
-import tailwindStyles from "@victorhalldev/react/theme.css?url";
+import libStyles from "@victorhalldev/react/theme.css?url";
 import type { CustomHandle } from "types";
+import twStyles from "./tw.css?url";
 import { cn } from "./utils/cn";
 import { getTheme } from "./utils/session.server";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: tailwindStyles },
+  { rel: "stylesheet", href: twStyles },
+  { rel: "stylesheet", href: libStyles },
   {
     rel: "icon",
     href: "https://res.cloudinary.com/davlbxibi/image/upload/v1716409614/remix-portfolio/byw1g36xmmfgv1di8m5h.png",
