5dd8f5bf1f6f192d96039dfb3390bdb1e67ffa86
VHall1 <victorhallpsn@hotmail.com>
2024-08-27 16:33:36 +0100

    finalise footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,9 @@
 import { SiGithub, SiLinkedin } from "@icons-pack/react-simple-icons";
-import { Link, useFetcher, useRouteLoaderData } from "@remix-run/react";
+import { useFetcher, useRouteLoaderData } from "@remix-run/react";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import type { loader } from "~/root";
 import type { action } from "~/routes/action.set-theme";
-import { Separator } from "./ui/separator";
 
 export function Footer() {
   const themeFetcher = useFetcher<typeof action>({ key: "theme" });
@@ -15,51 +14,50 @@ export function Footer() {
   const isDarkMode = theme === "dark";
 
   return (
-    <footer>
-      <div className="container py-8 lg:py-20 pb-20 px-4">
-        <Separator />
-        <div className="pt-8 flex items-center justify-between">
-          <p className="leading-7">Made with ♥️ by Victor Hall</p>
-          <div className="flex items-center">
-            <Link
-              className="text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-50 mr-4"
-              to="https://github.com/vhall1"
+    <footer className="container px-0 bg-accent lg:bg-background border-t">
+      <div className="container lg:px-0 py-8 flex items-center justify-between">
+        <p className="leading-7">
+          Made with ♥️ by <a href="https://github.com/vhall1">Victor Hall</a>
+        </p>
+        <div className="flex items-center">
+          <a
+            className="text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-50 mr-4"
+            href="https://github.com/vhall1"
+          >
+            <SiGithub className="h-5 w-5" />
+            <span className="sr-only">GitHub</span>
+          </a>
+          <a
+            className="text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-50 mr-1"
+            href="https://linkedin.com/in/vhall1/"
+          >
+            <SiLinkedin className="h-5 w-5" />
+            <span className="sr-only">LinkedIn</span>
+          </a>
+          <themeFetcher.Form
+            method="post"
+            action="/action/set-theme"
+            preventScrollReset
+          >
+            <input
+              type="hidden"
+              name="nextTheme"
+              value={isDarkMode ? "light" : "dark"}
+            />
+            <Button
+              size="icon"
+              variant="link"
+              className="text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-50"
+              type="submit"
+              aria-label="toggle colour scheme"
             >
-              <SiGithub className="h-5 w-5" />
-              <span className="sr-only">GitHub</span>
-            </Link>
-            <Link
-              className="text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-50 mr-1"
-              to="https://linkedin.com/in/vhall1/"
-            >
-              <SiLinkedin className="h-5 w-5" />
-              <span className="sr-only">LinkedIn</span>
-            </Link>
-            <themeFetcher.Form
-              method="post"
-              action="/action/set-theme"
-              preventScrollReset
-            >
-              <input
-                type="hidden"
-                name="nextTheme"
-                value={isDarkMode ? "light" : "dark"}
-              />
-              <Button
-                size="icon"
-                variant="link"
-                className="text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-50"
-                type="submit"
-                aria-label="toggle colour scheme"
-              >
-                {isDarkMode ? (
-                  <MoonIcon className="h-5 w-5" />
-                ) : (
-                  <SunIcon className="h-5 w-5" />
-                )}
-              </Button>
-            </themeFetcher.Form>
-          </div>
+              {isDarkMode ? (
+                <MoonIcon className="h-5 w-5" />
+              ) : (
+                <SunIcon className="h-5 w-5" />
+              )}
+            </Button>
+          </themeFetcher.Form>
         </div>
       </div>
     </footer>
